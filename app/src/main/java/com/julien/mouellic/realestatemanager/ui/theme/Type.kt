bec651ca820c0cd2858355a53b69ee5081de1afd
Julien Mouellic <ju.mouellic@gmail.com>
2025-08-02 14:08:09 +0200

    Set Ui theme

--- app/src/main/java/com/julien/mouellic/realestatemanager/ui/theme/Type.kt
+++ app/src/main/java/com/julien/mouellic/realestatemanager/ui/theme/Type.kt
@@ -1,12 +1,14 @@
 package com.julien.mouellic.realestatemanager.ui.theme
 
+// From Official Android Open Source Project Github
+// https://github.com/android/compose-samples
+
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
-// Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
         fontFamily = FontFamily.Default,
@@ -15,20 +17,4 @@ val Typography = Typography(
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
 )
