23143d7e7e1641744da9bb05334ab5e374e1e607
j-mouellic <ju.mouellic@gmail.com>
2025-08-22 12:14:09 +0200

    Add new route : search, get all, edit

--- app/src/main/java/com/julien/mouellic/realestatemanager/ui/navigation/PropertyNavHost.kt
+++ app/src/main/java/com/julien/mouellic/realestatemanager/ui/navigation/PropertyNavHost.kt
@@ -5,23 +5,26 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.julien.mouellic.realestatemanager.ui.screen.allproperties.AllPropertiesScreen
+import com.julien.mouellic.realestatemanager.ui.screen.createproperty.CreatePropertyScreen
 // import com.julien.mouellic.realestatemanager.ui.screen.allproperties.AllPropertiesScreen
 // import com.julien.mouellic.realestatemanager.ui.screen.createproperty.CreatePropertyScreen
 import com.julien.mouellic.realestatemanager.ui.screen.loancalculator.LoanCalculatorScreen
+
 // import com.julien.mouellic.realestatemanager.ui.screen.searchproperties.SearchPropertiesScreen
 
 @Composable
 fun PropertyNavHost(navController: NavHostController, modifier: Modifier) {
-    NavHost(navController, startDestination = "loan_calculator", modifier = modifier) {
-       // composable("all_properties") { AllPropertiesScreen(navController) }
-       // composable("create_property") { CreatePropertyScreen(null) }
-       // composable("search_properties") { SearchPropertiesScreen() }
+    NavHost(navController, startDestination = "all_properties", modifier = modifier) {
+        composable("all_properties") { AllPropertiesScreen(navController) }
+        composable("create_property") { CreatePropertyScreen(null) }
+        // composable("search_properties") { SearchPropertiesScreen() }
         composable("loan_calculator") { LoanCalculatorScreen() }
-        /*composable("edit_property/{propertyId}") { backStackEntry ->
+        composable("edit_property/{propertyId}") { backStackEntry ->
             val propertyId = backStackEntry.arguments?.getString("propertyId")?.toLong()
             if (propertyId != null) {
                 CreatePropertyScreen(propertyId)
             }
-        }*/
+        }
     }
 }
